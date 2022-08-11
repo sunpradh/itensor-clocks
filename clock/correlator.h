51f0cc5977af4da06a180c2aea735028e4c0ad97
Sunny Pradhan <sun.pradh@gmail.com>
2022-08-11 19:16:38 +0200

    other renaming of variables

--- clock/correlator.h
+++ clock/correlator.h
@@ -2,7 +2,7 @@
 #define __CLOCK_CORRELATOR_H
 
 #include "clock.h"
-#include "utils.h"
+#include "../utils/all.h"
 
 using Real = itensor::Real;
 using itensor::MPS;
@@ -54,40 +54,38 @@ ITensor clocks::compute_correlator_IT(
     const Interval & interv
 )
 {
-    int L = length(sites);
-    auto [start, end] = interv;
-
-    if (start < 0 || end > L || start >= end)
-        throw std::runtime_error("Incorrect range for disorder operator");
     if (!is_valid_op(op1))
         throw std::runtime_error("Unrecognized first operator for correlator");
     if (!is_valid_op(op2))
         throw std::runtime_error("Unrecognized second operator for correlator");
 
-    auto Op_start = op(sites, op1, start);
-    auto Op_stop  = op(sites, op2, end);
+    int L = length(sites);
+    auto [begin, end] = interv;
+    if (begin < 0 || end > L || begin >= end)
+        throw std::runtime_error("Incorrect position for correlator");
 
-    psi.position(start);
-    ITensor correl = psi(start);
+    auto op_first  = op(sites, op1, begin);
+    auto op_second = op(sites, op2, end);
+
+    psi.position(begin);
+    ITensor correl = psi(begin);
     // Contracts the operator at the start of the interval
-    correl *= Op_start;
+    correl *= op_first;
+
     // find the right link index of the bra <psi|
     // Primes both the site index and the link index and then contracts
-    correl *= dag(prime_inds(psi(start), "Site", rightLinkIndex(psi, start)));
+    correl *= dag(utils::prime_inds(psi(begin), "Site", rightLinkIndex(psi, begin)));
 
     // Contracts all the site inside the interval
-    for (int i=start+1; i<end; i++)
-    {
-        correl *= psi(i);
-        correl *= dag(prime(psi(i), "Link"));
-    }
+    for (int i=begin+1; i<end; i++)
+        correl *= psi(i) * dag(prime(psi(i), "Link"));
 
     // Contract the operators at the end of the interval
     correl *= psi(end);
-    correl *= Op_stop;
-    // find the left index of the bra <psi| and then contracts with evaluated correlator
-    correl *= dag(prime_inds(psi(end), "Site", leftLinkIndex(psi, end)));
+    correl *= op_second;
 
+    // find the left index of the bra <psi| and then contracts with evaluated correlator
+    correl *= dag(utils::prime_inds(psi(end), "Site", leftLinkIndex(psi, end)));
     // Contracts all the site at the right of the interval
 
     return correl;
