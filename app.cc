8ecbb530d1436b0a73e8b5eafb53625b46c91ffd
Sunny Pradhan <sun.pradh@gmail.com>
2022-08-11 19:17:44 +0200

    added examples

--- app.cc
+++ app.cc
@@ -2,25 +2,24 @@
 #include <vector>
 #include <cmath>
 #include <fstream>
+#include <numeric>
+
 // For benchmarking
 #include <chrono>
-using namespace std::chrono;
 
 #include "itensor/all.h"
 #include "clock/all.h"
-#include "clock/dump.h"
 
 using namespace itensor;
 using namespace clocks;
 using std::cout;
-// using std::cout;
 
 int main(int argc, char ** argv)
 {
     // Simulation setups
     constexpr uint N = 3; // clock order
-    constexpr uint L = 50; // chain length
-    auto sites = Clock<N>(L, {"ConserveQNs=", false});  // clock model
+    constexpr uint L = 10; // chain length
+    auto sites = Clock<N>(L, {"ConserveQNs=", true});  // clock model
 
     // Hamiltonian
     float h = 2.0;
@@ -28,14 +27,12 @@ int main(int argc, char ** argv)
 
     auto H = hamiltonian(sites, {
         "Kinetic",  -1,
-        "Transv",   -0.4,
+        "Transv",   -1.4,
         "PBC",      false
     });
 
-    //
     // Example of DMRG
-    //
-
+    //----------------------------------------
     // Sweeps setups
     auto sweeps = Sweeps(5);
     sweeps.maxdim() = 10, 20, 50, 100, 200;
@@ -49,11 +46,30 @@ int main(int argc, char ** argv)
     cout << "  > DMRG for ground state\n";
     auto [E0, psi] = dmrg(H, psi0, sweeps, {"Silent", true});
     cout << " E0 = " << E0 << "\n";
+    //----------------------------------------
 
-    // Benchmarking
-    auto f = [&](){ compute_orderC(sites, psi, "X"); };
-    auto bench = benchmark<20>(f);
-    bench.print_statistics<milliseconds>();
+    // Example order parameters
+    auto Xev = 0.5 * compute_orderC(sites, psi, "X", "Xdag");
+    auto Zev = compute_orderC(sites, psi, "Z");
+    cout << "<X>: " << Xev << "\n";
+    cout << "<Z>: " << Zev << "\n";
+
+    // Example disorder parameters
+    auto Xdisord = compute_disorderC(sites, psi, "X", {1, 5});
+    auto Zdisord = compute_disorderC(sites, psi, "Z", {1, 5});
+    cout << "Xdisord: " << Xdisord << "\n";
+    cout << "Zdisord: " << Zdisord << "\n";
+
+    // Example correlators
+    auto Xcorrel = compute_correlatorC(sites, psi, "X", "Xdag", {1, 5});
+    auto Zcorrel = compute_correlatorC(sites, psi, "Z", "Zdag", {1, 5});
+    cout << "Xcorrel: " << Xcorrel << "\n";
+    cout << "Zcorrel: " << Zcorrel << "\n";
+
+    // Example of benchmarking
+    auto f = [&](){ compute_orderC(sites, psi, "X", "Xdag"); };
+    auto bench = utils::benchmark<20>(f);
+    bench.print_statistics<std::chrono::milliseconds>();
 
     return 0;
 }
