dd0e4e9419cbabc56ed27e45792b345d609b6c42
Sunny Pradhan <sun.pradh@gmail.com>
2023-04-04 15:54:12 +0200

    sweeps parameter are read from external file

--- app.cc
+++ app.cc
@@ -7,33 +7,40 @@ namespace ut = utils;
 namespace cl = clocks;
 namespace sim = clocks::simulations;
 
-constexpr unsigned N = 2; // clock order
-constexpr unsigned n_points = 50; // number of points to compute
-constexpr unsigned n_excited = 2; // number of excited states to compute
+constexpr unsigned N = 3;          // clock order
+constexpr unsigned n_points = 151; // number of points to compute
+constexpr unsigned n_excited = 1;  // number of excited states to compute
 
 int main(int argc, char ** argv)
 {
-    // Sweeps setups
-    auto sweeps = it::Sweeps(5);
-    sweeps.maxdim() = 10, 20, 50, 100, 150;
-    sweeps.cutoff() = 1E-12;
-    sweeps.niter() = 4;
-    sweeps.noise() = 1e-7, 1e-8, 0.0;
-
     // Coupling range
-    auto couplings = ut::linspace(0.0, 2.0, n_points).to_array<n_points>();
+    auto couplings = ut::linspace(.0, 1., n_points).to_array<n_points>();
 
     // Chain lengths
-    auto lengths = {10, 20, 30};
+    // auto length = 10;
+    auto lengths = ut::range(12, 31, 2);
 
-    unsigned max_sector = N/2 + 1;
-    for (auto L : lengths) {
-        std::cout << "Clock, size = " << L << "\n";
+    // Sweeps setups
+    auto filename = "sweeps_input";
+    auto sweeps_input = it::InputGroup(filename, "sweeps_params");
+    auto sweeps_params = it::InputGroup(sweeps_input, "cutoff_based_nonoise");
+    auto sweeps = it::Sweeps(7, sweeps_params);
+
+    // Sector
+    // unsigned max_sector = N/2 + 1;
+    unsigned sector = 1;
+
+    for (auto length : lengths) {
+        std::cout << "Clock N=" << N << ", size = " << length << "\n";
         std::cout << "------------------------------------------------------------\n";
-        for (auto sector : ut::range(max_sector)) {
-            auto results = sim::Compute<N, n_points, n_excited>(L, sweeps, couplings).all(sector);
-            results.set_precision(12).to_csv(sim::output_csv_filename<N>(L, sector));
-        }
+        auto compute = sim::ComputeObservables<N, n_points, n_excited>(
+                length,
+                sweeps,
+                couplings,
+                {"NoExcited", true}
+            );
+        auto results = compute.all(sector);
+        results.set_precision(12).to_csv(sim::csv_filename<N>(length, sector));
         std::cout << "\n";
     }
 
