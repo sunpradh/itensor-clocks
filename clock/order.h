fb20f78fb01746dc372bde495ee72c5cb1fdf011
Sunny Pradhan <sun.pradh@gmail.com>
2023-05-11 02:57:24 +0200

    added bulk order parameters

--- clock/order.h
+++ clock/order.h
@@ -6,11 +6,14 @@
 
 #include "itensor/all.h"
 #include "clock.h"
+#include "../utils/ranges.h"
 
 template<typename T> using vector = std::vector<T>;
 
 /************************************************************/
 namespace clocks {
+
+namespace ut = utils;
 
 /// Computer order parameter
 /// yields real result
@@ -27,6 +30,26 @@ compute_order(
 template<unsigned N, typename... Args>
 complex
 compute_orderC(
+    const Clock<N> & sites,
+    it::MPS & psi,
+    Args... op_types
+);
+
+/// Computer order parameter
+/// yields real result
+template<unsigned N, typename... Args>
+double
+compute_bulk_order(
+    const Clock<N> & sites,
+    it::MPS & psi,
+    Args... op_types
+);
+
+/// Computer order parameter
+/// yields complex result
+template<unsigned N, typename... Args>
+complex
+compute_bulk_orderC(
     const Clock<N> & sites,
     it::MPS & psi,
     Args... op_types
@@ -64,6 +87,48 @@ complex compute_orderC(
             std::accumulate(expt.begin(), expt.end(), complex(.0))
         );
     return std::accumulate(results.begin(), results.end(), complex(.0)) / double(it::length(sites));
+}
+
+// Bulk order
+
+template<unsigned N, typename... Args>
+double compute_bulk_order(
+    const Clock<N> & sites,
+    it::MPS & psi,
+    Args... op_types
+) {
+    auto ops = vector<string>{op_types...};
+    vector<double> results;
+    results.reserve(ops.size());
+
+    int L = it::length(sites);
+    auto site_list = ut::range(L/4, (3*L/4) + 1).to_vector();
+
+    for (const auto & expt : it::expect(psi, sites, ops, site_list))
+        results.push_back(
+            std::accumulate(expt.begin(), expt.end(), .0)
+        );
+    return std::accumulate(results.begin(), results.end(), .0) / double(site_list.size());
+}
+
+template<unsigned N, typename... Args>
+complex compute_bulk_orderC(
+    const Clock<N> & sites,
+    it::MPS & psi,
+    Args... op_types
+) {
+    auto ops = vector<string>{op_types...};
+    vector<complex> results;
+    results.reserve(ops.size());
+
+    int L = it::length(sites);
+    auto site_list = ut::range(L/4, (3*L/4) + 1).to_vector();
+
+    for (const auto & expt : it::expectC(psi, sites, ops, site_list))
+        results.push_back(
+            std::accumulate(expt.begin(), expt.end(), complex(.0))
+        );
+    return std::accumulate(results.begin(), results.end(), complex(.0)) / double(site_list.size());
 }
 
 }
